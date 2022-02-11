4818dd56f0af200d99ed43961978a82817b0e761
Jesus Araujo <jesusaraujo.0699@gmail.com>
2022-02-10 20:04:12 -0500

    Header y sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+// @ts-ignore
+import Typewriter from 't-writer.js';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  
+
+  @ViewChild('asTitle') asTitle!: ElementRef;
+
+  constructor() { 
+    
+  }
 
   ngOnInit(): void {
+   
+  }
+
+  ngAfterViewInit(): void {
+    this.initEffect();
+  }
+
+  initEffect = () => {
+    // Obtener el elemento, (el target)
+    const target = this.asTitle.nativeElement;
+
+    const writer = new Typewriter(target, {
+      loop: true,
+      typeColor: 'white'
+    })
+    
+    writer
+      .type('Jesus Araujo')
+      .rest(500)
+      .start();
+              
   }
 
 }
